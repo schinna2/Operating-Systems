a5c10154ce8962d8ce3af5ab3c1c5c313d952094
csvb <csvb@CS550-schinna2.csvb.local>
2017-10-13 00:27:27 -0400

    Added files mysyscalls.c , mysyscall.c, syscall_64.tbl, readMe.txt

---
+++ mysyscall.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<unistd.h>
+#include<linux/unistd.h>
+#include<linux/time.h>
+int main(){
+struct timespec time;
+syscall(326, &time);
+printf("Syscall return value : %ld", time.tv_nsec);
+// printf("syscall return value :%d\n",x);
+return 0;
+}
+
---
+++ readMe.txt
@@ -0,0 +1,8 @@
+OPERATING SYSTEMS
+USER PROGRAM	:	mysyscall.c
+compile		:	gcc mysyscall.c
+Run		:	./a.out
+
+KERNEL PROGRAM	:	mysyscalls.c
+compile		:	gcc mysyscalls.c
+Run		:	./a.out
